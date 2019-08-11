e25830b3a14798ab8b20a9eb9186db4c9003dd1d
hilmizul <netspytux@gmail.com>
2019-08-11 10:38:05 +0700

    add: kelas & pegangan

--- master/models.py
+++ master/models.py
@@ -22,12 +22,24 @@ class Buku(models.Model):
     ('RPL', 'Rekayasa Perangkat Lunak'),
     ('TBSM', 'Teknik dan Bisnis Sepeda Motor'),
   )
+  kelas_choices = (
+    ('X', 'X (Sepuluh)'),
+    ('XI', 'XI (Sebelas)'),
+    ('XII', 'XII (Dua belas)'),
+  )
+  pegangan_choices = (
+    ('Guru', 'Guru'),
+    ('Siswa', 'Siswa'),
+    ('Guru/Siswa', 'Guru/Siswa'),
+  )
   kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
   kategori_khusus = models.CharField(max_length=4, choices=kategori_khusus_choices, blank=True)
   isbn = models.CharField(max_length=100)
   tanggal = models.DateTimeField(auto_now_add=True)
   kode_buku = models.CharField(max_length=40)
   judul = models.CharField(max_length=80)
+  kelas = models.CharField(max_length=15, choices=kelas_choices, blank=True, null=True)
+  pegangan = models.CharField(max_length=10, choices=pegangan_choices, blank=True, null=True)
   penulis = models.CharField(max_length=80)
   penerbit = models.ForeignKey(Penerbit, on_delete=models.CASCADE)
   stok = models.IntegerField()
